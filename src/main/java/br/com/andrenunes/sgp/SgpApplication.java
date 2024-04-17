d0d422371b4f3cc45cbfd6edddd780d85443dfd2
André Luiz <andre.doctors@gmail.com>
2024-04-17 18:43:44 -0400

    Feat: Implementacao de Produtos e associacao N para N

--- src/main/java/br/com/andrenunes/sgp/SgpApplication.java
+++ src/main/java/br/com/andrenunes/sgp/SgpApplication.java
@@ -1,7 +1,9 @@
 package br.com.andrenunes.sgp;
 
 import br.com.andrenunes.sgp.domain.Categoria;
+import br.com.andrenunes.sgp.domain.Produto;
 import br.com.andrenunes.sgp.repositories.CategoriaRepository;
+import br.com.andrenunes.sgp.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,8 @@ public class SgpApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgpApplication.class, args);
@@ -25,6 +29,18 @@ public class SgpApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll((Arrays.asList(p1, p2, p3)));
 	}
 }
