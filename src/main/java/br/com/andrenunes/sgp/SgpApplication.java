06996c83062d6b5e54d4c54b1445fb84845385f5
André Luiz <andre.doctors@gmail.com>
2024-04-17 16:43:57 -0400

    Feat: Instanciação de categoria

--- src/main/java/br/com/andrenunes/sgp/SgpApplication.java
+++ src/main/java/br/com/andrenunes/sgp/SgpApplication.java
@@ -1,13 +1,30 @@
 package br.com.andrenunes.sgp;
 
+import br.com.andrenunes.sgp.domain.Categoria;
+import br.com.andrenunes.sgp.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SgpApplication {
+public class SgpApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgpApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
